da4faf5ca28c1e7d72b82adf26b250f11fdd9ccb
kdiscua <156035034+kdiscua@users.noreply.github.com>
2024-01-21 18:23:18 -0600

    added search function using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-function handlerSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input-form");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "524a99a4fcd0df4388dto8e069b84440";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handlerSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Miami");
