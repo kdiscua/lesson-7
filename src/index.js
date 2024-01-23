c1673e544ea840a9d09230d903dac9c6e496d55a
kdiscua <156035034+kdiscua@users.noreply.github.com>
2024-01-22 19:55:35 -0600

    injected forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -52,7 +52,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">🌤️</div>
+  <div class="weather-forecast-temps">
+     <div class="weather-forecast-temp">
+       <strong>15°</strong>
+       </div>
+      <div class="weather-forecast-min">12°</div>
+          </div>
+          </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Miami");
+displayForecast();
